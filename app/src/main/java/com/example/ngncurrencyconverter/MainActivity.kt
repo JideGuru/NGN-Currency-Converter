1f3bac3f2b45f37d6ebe45dbdbad6d66fa169226
jideguru <guruliciousjide@gmail.com>
2021-06-24 12:53:46 +0100

    bugfix: Fixed Spinner issues

--- app/src/main/java/com/example/ngncurrencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/ngncurrencyconverter/MainActivity.kt
@@ -10,37 +10,47 @@ import android.widget.Spinner
 import com.example.ngncurrencyconverter.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     lateinit var binding: ActivityMainBinding
+    var selectedOption: String = "USD"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val spinner: Spinner = binding.currency
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currency_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.setSelection(0)
+        spinner.onItemSelectedListener = this
         binding.convert.setOnClickListener{ convertAmount()}
     }
 
     @SuppressLint("StringFormatInvalid")
     private fun convertAmount() {
         val stringInTextField = binding.enterAmount.text.toString()
-        val cost = stringInTextField.toDouble()
-        val total = cost/500
-        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
-        binding.rate.text= getString(R.string.total, formattedTotal)
-
-        val spinner: Spinner = binding.currency
-
-        ArrayAdapter.createFromResource(
-                this,
-                R.array.currency_array,
-                android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
+        if (stringInTextField.isNotEmpty()) {
+            val cost = stringInTextField.toDouble()
+            val total = cost/500
+            val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
+            binding.rate.text = getString(R.string.total, formattedTotal)
         }
     }
 
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
 
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+       selectedOption = parent?.getItemAtPosition(position).toString()
+    }
 }
 
 
